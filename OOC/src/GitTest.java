e5ec3b41593600dfced4b32cb322d44f6fd1b768
Tian_Tiange <1608858047@qq.com>
2021-05-18 15:49:42 +0800

    second version

---
+++ OOC/src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author TTG
+ * @Date 2021/5/18 15:49
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
